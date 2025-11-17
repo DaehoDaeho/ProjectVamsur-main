c50c69d280925e9a9811b7255b1462588f3c41fc
DaehoDaeho <relayer@naver.com>
2025-11-17 19:47:46 +0900

    .

---
+++ Assets/Scripts/Enemy/EnemyChase.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    public Transform target;
+    public Rigidbody2D body;    
+    public float stopDistance = 1.0f;
+
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.FindAnyObjectByType<GameManager>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(target == null)
+        {
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            if(playerObj != null)
+            {
+                target = playerObj.transform;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        bool canMove = false;
+        if (gameManager != null)
+        {
+            canMove = gameManager.IsPlaying();     
+        }
+
+        if(canMove == false)
+        {
+            return;
+        }
+
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector2 toTarget = (Vector2)(target.position - transform.position);
+        float dist = toTarget.magnitude;
+
+        if(dist <= stopDistance)
+        {
+            return;
+        }
+
+        Vector2 dir = toTarget.normalized;
+        //Vector2 dir = toTarget / dist;
+
+        Vector2 current = body.position;
+        Vector2 next = current + (dir * moveSpeed * Time.fixedDeltaTime);
+        body.MovePosition(next);
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    public void SetMoveSpeed(float value)
+    {
+        moveSpeed = value;
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour, IDamageable
+{
+    public float maxHealth = 100.0f;
+    public SpriteRenderer spriteRenderer;
+    public float hitFlashTime = 0.05f;
+
+    private float currentHealth;
+    private float hitFlashTimer;
+
+    private Color originalColor;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+
+        if(spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(hitFlashTimer > 0.0f)
+        if(hitFlashTimer < hitFlashTime)
+        {
+            //hitFlashTimer -= Time.deltaTime;
+            hitFlashTimer += Time.deltaTime;
+
+            //if(hitFlashTimer <= 0.0f)
+            if(hitFlashTimer >= hitFlashTime)
+            {
+                RestoreColor();
+            }
+        }
+    }
+
+    public void ApplyDamage(float amount)
+    {
+        currentHealth -= amount;
+
+        PlayHitFlash();
+
+        if(currentHealth <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void PlayHitFlash()
+    {
+        if(spriteRenderer != null)
+        {
+            spriteRenderer.color = Color.white;
+            //hitFlashTimer = hitFlashTime;
+            hitFlashTimer = 0.0f;
+        }
+    }
+
+    void RestoreColor()
+    {
+        if(spriteRenderer != null)
+        {
+            spriteRenderer.color = originalColor;
+        }
+    }
+
+    public void SetMaxHealth(float value)
+    {
+        maxHealth = value;
+        currentHealth = maxHealth;
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+}
